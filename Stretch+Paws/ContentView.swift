0717e9f2bae69d9a94dc7ca761aedc3a3c8a3467
Hugo César García <hcgarcia.alv@gmail.com>
2022-03-22 13:50:37 -0500

    Content View Styled

--- Stretch+Paws/ContentView.swift
+++ Stretch+Paws/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    init() {
+        UITableView.appearance().backgroundColor = UIColor(named: "Secondary")
+    }
+    
+    let poses = Poses()
     var body: some View {
         NavigationView {
-            List {
-                NavigationLink(destination: DetailView()) {                Text("Downward-facing Dog")
-}
-                Text("Standing Forward Fold")
-                Text("Tree Pose")
-                }
+            List(poses.poseData) { pose in
+                NavigationLink(destination: DetailView()) {
+                    Image(pose.icon)
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                    Text(pose.name)
+                        .fontWeight(.medium)
+                        .font(.title3)
+                        .padding(.leading, 20)
+                        }
+                .padding(5)
+                .listRowBackground(Color("Secondary"))
+                    }
                 .listStyle(.grouped)
                 .navigationBarTitle("Stretch + Paws")
         }
