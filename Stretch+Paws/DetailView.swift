f3b09d7a5a6744f10932545ba45dd23f19e1c637
Hugo César García <hcgarcia.alv@gmail.com>
2022-03-25 14:10:54 -0500

    Timer countdown complete

--- Stretch+Paws/DetailView.swift
+++ Stretch+Paws/DetailView.swift
@@ -64,6 +64,7 @@ struct DetailView_Previews: PreviewProvider {
 }
 
 struct TimerPanelView: View {
+    @StateObject var yogaTimer = YogaTimer()
     @Binding var timerOpen: Bool
     var body: some View {
         // if the timer panel is closed, show the timer closed view.
@@ -72,7 +73,9 @@ struct TimerPanelView: View {
         VStack {
             Spacer()
             VStack {
-                timerOpen ? AnyView(TimerOpenView()) : AnyView(TimerClosedView())
+                timerOpen ?
+                AnyView(TimerOpenView(yogaTimer:yogaTimer)) :
+                AnyView(TimerClosedView())
             }
                 .foregroundColor(Color("Secondary"))
                 .frame(maxWidth: .infinity, maxHeight: timerOpen ? 400 : 80)
@@ -87,6 +90,7 @@ struct TimerPanelView: View {
 }
 
 struct TimerOpenView: View {
+    @ObservedObject var yogaTimer: YogaTimer
     var body: some View {
         VStack {
             Text("Hold that pose")
@@ -95,11 +99,12 @@ struct TimerOpenView: View {
             Text("Try staying in this pose for 30 seconds. If you need to come out sooner, that's ok.")
                 .multilineTextAlignment(.center)
             Spacer()
-            Text("00:30")
+            Text(yogaTimer.timerDuration < 10 ? "00:0\(yogaTimer.timerDuration)" : "00:\(yogaTimer.timerDuration)")
                 .font(.system(size: 96))
             Spacer()
             Button {
                 // do something
+                yogaTimer.startTimer()
             } label: {
                 Text("Start the timer")
             }
